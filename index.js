6b7f1e55595c7a3b8c8e91d8e3a8c491b2111fe3
Emma Gummeson <emma.gummeson@elev.ga.ntig.se>
2021-05-17 15:02:03 +0200

    finished

---
+++ index.js
@@ -0,0 +1,18 @@
+function termsAndConditions() {
+    var TAC = document.querySelector(".TACtxt")
+    if (TAC.style.display === "none") {
+        TAC.style.display = "block";
+    }
+    else {
+        TAC.style.display = "none";
+    }
+}
+function aboutClick() {
+    var aC = document.querySelector(".aboutUs")
+    if (aC.style.display === "none") {
+        aC.style.display = "block";
+    }
+    else {
+        aC.style.display = "none";
+    }
+}
